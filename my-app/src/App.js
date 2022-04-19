bbbe611a8009d4c05790ac5e4bae01c9b33a8d77
unknown <n.rahnama1996@gmail.com>
2022-04-19 11:22:52 +0430

    the first push

---
+++ my-app/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Basket from './components/Basket';
+import data from './data';
+
+
+function App() {
+
+  const [cartItems,setCartItems] = useState([])
+   
+  const {products} = data;
+
+  const onAdd = (product) => {
+    const exist = cartItems.find(x => x.id ===product.id);
+    if(exist){
+      setCartItems (cartItems.map(x => x.id ===product.id ?
+         {...exist, qty: exist.qty +1 }: x))
+    }else{
+      setCartItems([...cartItems, {...product,qty :1}])
+    }
+  }
+
+   const onRemove = (product) => {
+    const exist = cartItems.find (x => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id))
+    }else{
+      setCartItems(
+        cartItems.map((x) => x.id === product.id ?
+        {...exist, qty: exist.qty -1 }: x
+        )
+      )
+    }
+   }
+
+  return (
+    <div className="App">
+    <Header countCartItems={cartItems.length}/>
+
+    <div className='row'>
+      <Main onAdd={onAdd}  products={products} />
+      <Basket onAdd={onAdd} onRemove={onRemove} cartItems={cartItems}/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+import Product from './Product'
+
+
+const Main = (props) => {
+
+    const {products,onAdd} = props
+
+  return (
+    <main className='block col-1'>
+    <h2>Products</h2>
+
+    <div className='row'>
+        {products.map((product) => (
+            <Product key={product.id} product={product} onAdd={onAdd} />
+        ))}
+    </div>
+    </main>
+  )
+}
+
+export default Main
